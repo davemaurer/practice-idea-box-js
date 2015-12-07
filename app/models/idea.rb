f2c68d33c66d7b2684576961f59d448e347198a6
David Maurer <davemaurermail@gmail.com>
2015-12-07 08:09:22 -0700

    changed quality column to string defaulting to swill
    so we can not use enum. Now messages can be given for an invalid quality change.
    
    Added tests for quality validation and validations to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,4 @@
 class Idea < ActiveRecord::Base
   validates_presence_of :title, :body
-
-  enum quality: %w(swill plausible genius)
+  validates :quality, inclusion: { in: %w(swill plausible genius) }
 end
