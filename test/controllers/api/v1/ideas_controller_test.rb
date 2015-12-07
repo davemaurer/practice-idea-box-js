f788514cf15682a88a5a908f08a3d8089b93e392
David Maurer <davemaurermail@gmail.com>
2015-12-07 07:01:43 -0700

    added json_response helper and test for index returning all ideas

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -5,4 +5,16 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
     get :index, format: :json
     assert_response :success
   end
+
+  test "index returns an array of records" do
+    get :index, format: :json
+
+    assert_kind_of Array, json_response
+  end
+
+  test "index returns the correct number of ideas" do
+    get :index, format: :json
+
+    assert_equal Idea.count, json_response.count
+  end
 end
