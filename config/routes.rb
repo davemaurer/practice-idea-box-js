eaa7301af7bf45e10de156dda843fa85b0da23be
David Maurer <davemaurermail@gmail.com>
2016-01-04 07:17:57 -0700

    add root page and test for loading it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static#main'
+
   namespace :api do
     namespace :v1 do
       resources :ideas
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   fixtures :all
@@ -10,4 +11,9 @@ class ActionController::TestCase
   def json_response
     JSON.parse response.body
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Rails.application.routes.url_helpers
 end
