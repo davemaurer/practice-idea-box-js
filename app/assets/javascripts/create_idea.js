9079af67f0d0db291c580ab70c95ac41f75847eb
David Maurer <davemaurermail@gmail.com>
2016-02-25 12:07:10 -0700

    fixed error messages not working with tests
    by adding errorMessages variable and tying it to new-idea-messages
    in the view

--- app/assets/javascripts/create_idea.js
+++ app/assets/javascripts/create_idea.js
@@ -1,16 +1,22 @@
-var newIdeaTitle, newIdeaBody;
+var newIdeaTitle, newIdeaBody, errorMessages;
 
 $(document).ready(function () {
   newIdeaTitle = $('.new-idea-title');
   newIdeaBody = $('.new-idea-body');
+  errorMessages = $('.new-idea-messages');
 
   $('.new-idea-submit').on('click', createIdea);
 });
 
 function createIdea(event) {
   event.preventDefault();
+  clearErrors();
   IdeaRepository.create(getNewIdea())
                 .fail(renderError);
+}
+
+function clearErrors() {
+  return errorMessages.html('');
 }
 
 function getNewIdea() {
@@ -18,4 +24,8 @@ function getNewIdea() {
     title: newIdeaTitle.val(),
     body: newIdeaBody.val()
   }
+}
+
+function renderError() {
+  errorMessages.text('Title and/or body cannot be blank.');
 }
--- test/integration/creating_ideas_test.rb
+++ test/integration/creating_ideas_test.rb
@@ -37,4 +37,16 @@ class LoadingIdeasTest < ActionDispatch::IntegrationTest
 
     assert page.find('.new-idea-messages').has_content? 'Title and/or body cannot be blank.'
   end
+
+  test "it removes the error on subsequent submission" do
+    page.click_button "Submit Idea"
+
+    wait_for_ajax
+
+    page.fill_in "idea[title]", with: "Special Idea"
+    page.fill_in "idea[body]", with: "World domination"
+    page.click_button "Submit Idea"
+
+    refute page.find('.new-idea-messages').has_content? 'Title and/or body cannot be blank.'
+  end
 end
